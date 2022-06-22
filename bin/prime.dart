74bfb2659b0a7482e371e6f1f262aecf8aad78a7
tarik <tba@gmail.com>
2022-06-22 22:02:28 +0600

    check primr or not

--- bin/prime.dart
+++ bin/prime.dart
@@ -1,1 +1,21 @@
 //Ask the user for a number and determine whether the number is prime or not.
+
+import 'dart:io';
+
+void main() {
+  print("Enter your number for check: ");
+  var input = int.parse(stdin.readLineSync()!);
+  List divisor = [];
+
+  for (int i = 1; i <= input; i++) {
+    if (input % i == 0) {
+      divisor.add(i);
+    }
+  }
+
+  if (divisor.length > 2) {
+    print('$input is not a prime number');
+  } else {
+    print('$input is a prime number');
+  }
+}
